06b291a216e3fdd70002487ef19d8a95adeabd75
Harrison <jiksdevops@gmail.com>
2022-09-01 20:51:24 +0200

    Greets user based on current time

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import profileicon from '../components/assets/profileownerimg.svg';
 
+function Greet(hours) {
+	let greet;
+	hours = new Date().getHours();
+	if (hours < 12) greet = 'Good Morning';
+	else if (hours >= 12 && hours <= 17) greet = 'Good Afternoon';
+	else if (hours >= 17 && hours <= 24) greet = 'Good Evening';
+	return greet;
+}
+
 const DashboardHeader = () => {
 	const options = {
 		weekday: 'long',
@@ -10,11 +19,16 @@ const DashboardHeader = () => {
 	};
 	let currentTime = new Date().toLocaleString('en-US', options);
 	let user = JSON.parse(localStorage.getItem('user'));
+
 	return (
 		<div className="NavHeader">
 			<div className="dashboardheader-text NavHeader-margin">
-				<h1>Welcome, {user.name} !</h1>
-				<h6>{currentTime}</h6>
+				<h1>
+					{Greet()}, {user.name} !
+				</h1>
+
+				<h5>{currentTime}</h5>
+
 				<h4>What delicious meal are you craving today?</h4>
 			</div>
 			<div className="dashboardheader-logo NavHeader-margin">
